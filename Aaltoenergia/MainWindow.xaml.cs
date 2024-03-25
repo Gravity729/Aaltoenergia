6701afa6f68af0f7b2466cfbc40b81015ab0cb99
marus <marus@LAPTOP-J3NOIFB9>
2024-03-25 20:45:07 +0300

    Добавьте файлы проекта.

---
+++ Aaltoenergia/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Aaltoenergia.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Aaltoenergia
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Authorization_OnClick(object sender, RoutedEventArgs e)
+        {
+            Authorization wAuthorization = new();
+            wAuthorization.Show();
+            this.Hide();
+        }
+        private void Registration_OnClick(object sender, RoutedEventArgs e)
+        {
+            Registration wRegistration = new();
+            wRegistration.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Aaltoenergia/View/Registration.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Aaltoenergia.View
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+        private void Main_OnClick(object sender, RoutedEventArgs e)
+        {
+            MainWindow wAuthorization = new();
+            wAuthorization.Show();
+            this.Hide();
+        }
+    }
+}
